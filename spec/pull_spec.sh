16b34db1ff07069832fcfdd5ec2d58b9b26a733f
Omar Al-Ithawi <i@omardo.com>
2023-04-10 23:03:53 +0300

    test: add tests for multiple directories in pull_spec.sh
    Test cases was missing multiple directory tests, this commit adds them.
    
    Refs: FC-0012 OEP-58

--- spec/pull_spec.sh
+++ spec/pull_spec.sh
@@ -41,10 +41,19 @@ Describe 'Pull with directory param'
       echo "rm $@"
   }
 
-  setup() { pull_directory="pull_directory:local_dir" pull_repository="pull_repository" pull_branch="pull_branch"; VERBOSE=1; }
+  mkdir() {
+    echo "mkdir $@"
+  }
+
+  cd() {
+    echo "cd $@"
+  }
+
+
+  setup() { pull_directory="pull_directory:local_dir pull_dir2:new/local_dir2" pull_repository="pull_repository" pull_branch="pull_branch"; VERBOSE=1; }
   BeforeEach 'setup'
 
-  It 'calls everything properly'
+  It 'calls everything properly with multiple directories'
     When call pull_translations
     The output should equal 'Creating a temporary Git repository to pull translations into "./translations_TEMP"...
 git clone --branch=pull_branch --filter=blob:none --no-checkout --depth=1 https://github.com/pull_repository.git translations_TEMP
@@ -53,14 +62,20 @@ Done.
 Setting git sparse-checkout rules...
 git sparse-checkout set --no-cone !*
 git sparse-checkout add pull_directory/**
+git sparse-checkout add pull_dir2/**
 Done.
 Pulling translation files from the repository...
 git checkout HEAD
 rm -rf .git
 cd ..
+mkdir -p local_dir
 Done.
 Copying translations from "./translations_TEMP/pull_directory" to "./local_dir"...
 cp -r ./translations_TEMP/pull_directory/* local_dir/
+mkdir -p new/local_dir2
+Done.
+Copying translations from "./translations_TEMP/pull_dir2" to "./new/local_dir2"...
+cp -r ./translations_TEMP/pull_dir2/* new/local_dir2/
 Done.
 Removing temporary directory...
 rm -rf translations_TEMP
